ff716833a750a15ca88f2b90c18d6efb60e30bac
y242yang <ann.ymyang@gmail.com>
2026-06-03 19:39:29 -0700

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -19,6 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${outfit.variable} h-full antialiased`}>
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-7FPFTJMVRS" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7FPFTJMVRS');
+        `}</Script>
+      </head>
       <body className="min-h-full flex flex-col bg-[#08080f] text-white">
         {children}
       </body>
